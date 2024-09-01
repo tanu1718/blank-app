46e0943240b801937737fb5ab8e4eee6f1bc3555
tanu1718 <trana01@g.syr.edu>
2024-09-01 03:25:28 +0000

    changes to streamlit_app.py

---
+++ app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("New title")
+st.write("New app!!!")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
+import pandas as pd
 import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+df = pd.DataFrame({
+'first column': [1, 2, 3, 4],
+'second column': [10, 20, 30, 40]
+})
+st.write(df)
+
+option = st.selectbox(
+'Which number do you like best?',
+df['first column'])
+'You selected: ', option
+
+add_selectbox = st.sidebar.selectbox(
+'How would you like to be contacted?',
+('Email', 'Home phone', 'Mobile phone')
 )
